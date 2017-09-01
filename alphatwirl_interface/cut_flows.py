b8f1f7847495689ff55998f5862002f222c7ea56
kreczko <lkreczko@googlemail.com>
2017-09-01 16:24:31 +0100

    adding cutflow without output file

--- alphatwirl_interface/cut_flows.py
+++ alphatwirl_interface/cut_flows.py
@@ -1,15 +1,35 @@
 import alphatwirl
 
-def cut_flow_with_counter(cut_flow, cut_flow_summary_filename):
-    eventSelection = alphatwirl.selection.build_selection(
-        path_cfg = cut_flow,
-        AllClass = alphatwirl.selection.modules.AllwCount,
-        AnyClass = alphatwirl.selection.modules.AnywCount,
-        NotClass = alphatwirl.selection.modules.NotwCount
+
+def _build_selection(cut_flow):
+    return alphatwirl.selection.build_selection(
+        path_cfg=cut_flow,
+        AllClass=alphatwirl.selection.modules.AllwCount,
+        AnyClass=alphatwirl.selection.modules.AnywCount,
+        NotClass=alphatwirl.selection.modules.NotwCount
     )
+
+
+def _file_collector(cut_flow_summary_filename):
     resultsCombinationMethod = alphatwirl.collector.ToTupleListWithDatasetColumn(
-        summaryColumnNames = ('depth', 'class', 'name', 'pass', 'total')
+        summaryColumnNames=('depth', 'class', 'name', 'pass', 'total')
     )
-    deliveryMethod = alphatwirl.collector.WriteListToFile(cut_flow_summary_filename)
-    collector = alphatwirl.loop.Collector(resultsCombinationMethod, deliveryMethod)
+    deliveryMethod = alphatwirl.collector.WriteListToFile(
+        cut_flow_summary_filename)
+    collector = alphatwirl.loop.Collector(
+        resultsCombinationMethod, deliveryMethod)
+    return collector
+
+
+def cut_flow_with_counter(cut_flow, cut_flow_summary_filename):
+    eventSelection = _build_selection(cut_flow)
+    collector = _file_collector(cut_flow_summary_filename)
+
+    return [(eventSelection, collector)]
+
+
+def cut_flow(cut_flow):
+    eventSelection = _build_selection(cut_flow)
+    collector = alphatwirl.loop.NullCollector()
+
     return [(eventSelection, collector)]
