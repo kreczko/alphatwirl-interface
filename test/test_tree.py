059a880cfb50a819050bea9f833a8a17261f5723
kreczko <lkreczko@googlemail.com>
2017-09-04 11:45:27 +0100

    added tests for bug in Tree.scan

---
+++ test/test_tree.py
@@ -0,0 +1,25 @@
+import alphatwirl as at
+from alphatwirl_interface import Table
+from alphatwirl_interface.tree import _complement_tblcfg_with_default
+from nose.tools import assert_equal
+
+
+def test_vanishing_attribute_names():
+    '''
+        Unit test for a bug: disappearing valAttrNames
+    '''
+    columns = ('NJet', 'di_muon_mass')
+    tables = [
+        Table(
+            input_values=columns,
+        ),
+    ]
+
+    default_cfg = dict(
+        keyAttrNames=(),
+        sort=False,
+        summaryClass=at.summary.Scan,
+        outFile=False,
+    )
+    tables = _complement_tblcfg_with_default(tables, default_cfg)
+    assert_equal(tables[0]['valAttrNames'], columns)
