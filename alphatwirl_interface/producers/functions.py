089549197f54f8ae3214c58ba866ffb15a8c4eb3
Luke Kreczko <lkreczko@googlemail.com>
2018-02-27 19:30:45 +0000

    added tests and documentation for function-based producers

--- alphatwirl_interface/producers/functions.py
+++ alphatwirl_interface/producers/functions.py
@@ -4,6 +4,21 @@ from .basic import BasicProducer
 
 
 class NewFromFunction(BasicProducer):
+    '''
+        Generic producer to calculate inputs from functions.
+        The passed function needs to return a sequence.
+        Can either be used directly, e.g.
+
+          met_ht_ratio = NewFromFunction(
+            'met_ht_ratio',
+            inputs=['MET', 'HT'],
+            function=np.divide,
+          )
+
+        or to implement specialisations as classes, see
+         - alphatwirl_interface.functions.Divide
+         - alphatwirl_interface.functions.TransverseMomentum
+    '''
 
     def __init__(self, outputName, inputs, function):
         super(NewFromFunction, self).__init__(outputName)
@@ -27,12 +42,25 @@ class NewFromFunction(BasicProducer):
 
 
 class Divide(NewFromFunction):
+    '''
+        Divides two inputs by each other (can be vectors)
+    '''
 
     def __init__(self, outputName, inputs):
         super(self.__class__, self).__init__(outputName, inputs, np.divide)
 
 
 class TransverseMomentum(NewFromFunction):
+    '''
+        Calculates the transverse momentum of particles given their
+        momenta in the x- and y-plane, e.g.
+
+          muon_pt = TransverseMomentum(
+            outputName='muon_pt',
+            inputs=['Muon_Px', 'Muon_Py'],
+          )
+
+    '''
 
     def __init__(self, outputName, inputs):
         super(self.__class__, self).__init__(
---
+++ test/producers/test_functions.py
@@ -0,0 +1,63 @@
+from collections import namedtuple
+import pytest
+import numpy as np
+from alphatwirl_interface.producers import TransverseMomentum, Divide, NewFromFunction
+
+from .fixtures import example_event
+
+
+@pytest.mark.usefixtures("example_event")
+def test_new_from_function(example_event):
+    e = example_event
+    new_attr = 'sin'
+    p = NewFromFunction(
+        new_attr,
+        inputs=['Muon_Px'],
+        function=np.sin,
+    )
+    # would usually be called in alphatwirl
+    p.begin(e)
+    p.event(e)
+    # manual calculation
+    expected = np.sin(e.Muon_Px)
+    assert hasattr(example_event, new_attr)
+    for ratio, exp_ratio in zip(getattr(e, new_attr), expected):
+        assert ratio == exp_ratio
+
+
+@pytest.mark.usefixtures("example_event")
+def test_divide(example_event):
+    e = example_event
+    new_attr = 'px_py_ratio'
+    p = Divide(
+        new_attr,
+        inputs=['Muon_Px', 'Muon_Py'],
+    )
+    # would usually be called in alphatwirl
+    p.begin(e)
+    p.event(e)
+    # manual calculation
+    expected = np.divide(e.Muon_Px, e.Muon_Py)
+    # test
+    assert hasattr(example_event, new_attr)
+    for ratio, exp_ratio in zip(getattr(e, new_attr), expected):
+        assert ratio == exp_ratio
+
+
+@pytest.mark.usefixtures("example_event")
+def test_transverse_momentum(example_event):
+    e = example_event
+    new_attr = 'muon_pt'
+    p = TransverseMomentum(
+        new_attr,
+        inputs=['Muon_Px', 'Muon_Py'],
+    )
+    # would usually be called in alphatwirl
+    p.begin(e)
+    p.event(e)
+    # manual calculation
+    expected = np.sqrt(np.square(e.Muon_Px) + np.square(e.Muon_Py))
+    # test
+    assert hasattr(example_event, new_attr)
+    for mu_pt, exp_pt in zip(getattr(e, new_attr), expected):
+        assert mu_pt == exp_pt
